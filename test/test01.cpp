99bf7b0e8f3a066dc36e4491c07de395dbbd47f1
Evgeny Zhdanovskiy <7323@list.ru>
2016-01-06 22:02:29 +0300

    fix minus test

--- test/test01.cpp
+++ test/test01.cpp
@@ -2,10 +2,10 @@
 
 #include <gtest/gtest.h>
 
-TEST(SumTest, Sum) {
-  EXPECT_EQ(5, sum(2, 3));
+TEST(OperationsTest, Sum) {
+  EXPECT_EQ(5, sum(3, 2));
 }
 
-TEST(SumTest, Minus) {
-  EXPECT_EQ(5, minus(2, 3));
+TEST(OperationsTest, Minus) {
+  EXPECT_EQ(1, minus(3, 2));
 }
